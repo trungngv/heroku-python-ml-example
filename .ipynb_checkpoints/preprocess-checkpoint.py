c59d89726c805d887f4586f0228e854d4dfee8a7
Trung Nguyen <trung.ngvan@gmail.com>
2018-09-04 20:52:04 +1000

    example classification model as web app

---
+++ .ipynb_checkpoints/preprocess-checkpoint.py
@@ -0,0 +1,7 @@
+import re
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    return text.split()
---
+++ app.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request
+
+import json
+import logging
+import os
+
+from preprocess import tokenizer
+from sklearn.externals import joblib
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+MODEL = os.path.join(APP_ROOT, 'classifier.pkl')
+
+PORT = 5000
+
+app = Flask(__name__)
+logging.basicConfig(filename='movie_classifier.log', level=logging.DEBUG)
+model = joblib.load(MODEL)
+label = {0:'negative', 1:'positive'}
+
+
+@app.route('/')
+def home():
+    return 'It works.'
+
+
+def predict(model, text):
+    return label[model.predict([text])[0]]
+
+
+@app.route('/review', methods=['GET'])
+def extract():
+    """Return the movie review sentiment score.
+    
+    Returns a JSON object :
+    {
+         "sentiment": "positive"
+    }
+    """
+    if request.method == 'GET':
+        description = request.args.get('text', '')
+        
+        result = {
+            'sentiment': predict(model, description)
+        }
+        return json.dumps(result) 
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=PORT)
+
